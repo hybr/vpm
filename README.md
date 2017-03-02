9b373a5f40bfba462deb5d08bbf8199cb4eb6aab
Yogesh Sharma <sharma.yogesh.1234@gmail.com>
2017-03-02 06:49:22 +0530

    Initial commit with starter kit
    Change-Id: Ie19135c0a2841a5be5858fe1b962dd00eaa530a7

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# vpm
-Vue, python and montgodb
+# Basic Authentication Example Built With Phoenix, Vue 2.0 and Vue Router 2.0
+
+****Sorry for not having the commit history of this project, at the beginning I didn't realize that I was going to upload it to Github***
+
+To start this application locally:
+
+* Make sure you have installed _Elixir_, _Erlang_, _Phoenix_, _Node_ and _PostgreSQL_
+* Install the Mix dependencies: `$ cd server/ && mix deps.get`
+* Create, migrate and seed the development database: `$ mix ecto.setup`
+* Start the Phoenix Server: `$ mix phoenix.server` or `$ iex -S mix phoenix.server`
+* Install the NPM packages `$ cd ../client/  && npm install`
+* Start the Express development server: `$ npm run dev`
+
+To build the Vue.js project for production:
+* In the client directory, run `$ npm run build` (this will execute the `build/build.js` script and will copy the produced files to the `server/priv/js/` and `server/priv/css/` directories)
+
+To deploy this application on Heroku:
+* Follow this [guide](http://www.phoenixframework.org/docs/heroku) in the Phoenix website.
+
+Next Steps:
+* Deployment using Docker and Docker-Compose.
---
+++ client/src/components/Home.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="form hello-view">
+    <form class="giphy-form" @submit.prevent="searchGIF()">
+      <input type="text" v-model="query" placeholder="Search all the GIFs">
+      <button type="submit">Search</button>
+    </form>
+    <div class="horizontal">
+      <div class="vertical">
+        <img class="welcome-gif" :src="image"/>
+      </div>
+    </div>
+    <p class="message home-logout-message">Already tired?
+      <a href="/login" @click.prevent="logout()">Get Out!</a>
+    </p>
+  </div>
+</template>
+
+<script>
+import auth from '../auth'
+import giphy from '../giphy'
+
+export default {
+
+  data: () => {
+    return {
+      user: {},
+      image: '',
+      query: ''
+    }
+  },
+
+  beforeCreate(){
+    auth.currentUser(this)
+    giphy.getGifByQuery(this, "welcome")
+  },
+
+  methods: {
+    searchGIF(){
+      const previousImage = this.image
+      this.image = ""
+      giphy.getGifByQuery(this, this.query, previousImage)
+      this.query = ""
+    },
+
+    logout(){
+      auth.logout(this, { headers: auth.getAuthHeader() })
+    }
+  }
+}
+</script>
+<style scoped>
+.hello-view {
+  max-width: 400px;
+  margin: 50px auto 0 auto;
+}
+.hello-view .home-logout-message {
+  font-size: 15px;
+}
+.welcome-gif {
+  max-width: 100%;
+  max-height: 100%;
+}
+.horizontal {
+  display: flex;
+  justify-content: center;
+  min-height: 350px;
+}
+.vertical {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+.giphy-form {
+  max-width: 400px;
+  margin: 0 auto 35px auto;
+}
+.giphy-form input {
+  font-size: 17px;
+  letter-spacing: 1px;
+  height: 52px;
+}
+@media only screen
+  and (min-device-width: 320px)
+  and (max-device-width: 480px)
+  and (-webkit-min-device-pixel-ratio: 2)
+  and (orientation: portrait) {
+    .horizontal {
+      min-height: 200px;
+    }
+}
+</style>
